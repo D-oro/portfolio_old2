8d46bc0804152a6d4f44f542bf23f91cda8b99e6
D-oro <107217252+D-oro@users.noreply.github.com>
2022-12-19 13:35:26 +0000

    added components to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,14 @@
 import './Accordion.css'
+import { Image} from './Image';
+import first from '../pictures/first.png'
+import { Textsnippet } from './Textsnippet';
+import { Hyperlink } from './Hyperlink';
 
 export const Accordion = () => {
 
     const expandOne = () =>{
         document.getElementById("1").hidden = false;
     }
-
     const collapseOne = () =>{
         document.getElementById("1").hidden = true;
     }
@@ -13,7 +16,6 @@ export const Accordion = () => {
     const expandTwo = () =>{
         document.getElementById("2").hidden = false;
     }
-
     const collapseTwo = () =>{
         document.getElementById("2").hidden = true;
     }
@@ -21,7 +23,6 @@ export const Accordion = () => {
     const expandThree = () =>{
         document.getElementById("3").hidden = false;
     }
-
     const collapseThree = () =>{
         document.getElementById("3").hidden = true;
     }
@@ -29,21 +30,29 @@ export const Accordion = () => {
     return(
         <div className="accordion-wrapper">
             <button className="accordion" onClick={expandOne}>Section 1</button>
-            <div id="1" hidden>
+            <div id="1" hidden >
+                <div className="expanded-accordion">
                 <button onClick={collapseOne} className="arrow"> ⌃ </button>
-                <p>blabla bla text that is not latin</p>
+                <Image alt="screenshot" image={first} />
+                <Textsnippet text="blablablaaaa" />
+                <Hyperlink hyperlink="https://d-oro.github.io/portfolio" linktext="→ portfolio website"/>
+                </div>
             </div>
 
             <button className="accordion" onClick={expandTwo}>Section 2</button>
             <div id="2" hidden>
+                <div className="expanded-accordion">
                 <button onClick={collapseTwo} className="arrow"> ⌃ </button>
                 <p>blabla text</p>
+                </div>
             </div>
 
             <button className="accordion" onClick={expandThree}>Section 2</button>
             <div id="3" hidden>
+                <div className="expanded-accordion">
                 <button onClick={collapseThree} className="arrow"> ⌃ </button>
                 <p>blabla text</p>
+                </div>
             </div>
         </div>
     )
