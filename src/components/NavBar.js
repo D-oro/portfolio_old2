9b768019a12e8fa2dc5ae06ede5ec012632f61e1
D-oro <107217252+D-oro@users.noreply.github.com>
2023-01-01 17:47:00 +0000

    navigation fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export const NavBar=()=>{
 
     return(
         <nav>
-            <Link to="/" className="navlink">Home</Link>
+            <Link to="/portfolio2.0/" className="navlink">Home</Link>
             <Link to="/projects" className="navlink">Projects</Link>
             <Link to="/skills" className="navlink">Skills</Link>
             <Link to="/contact" className="navlink">Contact</Link>
