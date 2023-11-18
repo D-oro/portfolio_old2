8f9f058f48782f869f96af45df87be768d0268b4
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-18 16:26:05 +0000

    rem to pc

---
+++ src/ToDo.md
@@ -0,0 +1,12 @@
+### To Do
+
+- DONE change px to rem
+- better title
+- better navbar
+- accessibility check
+- mostly html? 
+- new colour scheme
+- inaccessible website for non disabled people
+- new intro
+- update skills
+- how to section
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,11 @@ export const Home=()=>{
     return(
        <main>
             <div clasName="homewrapper">
-                <Textsnippet text="Hello and welcome to my portfolio! My name is Doro and I am a software developer with a unique background in psychology.
-                After earning my degree in psychology and working in mental health, I decided to make a career change and pursue my passion for technology." />
+                <h1>Accessible Coding</h1>
+                <Textsnippet text="Hello and welcome! This is the side project of a software developer passionate about accessible code!" />
 
-                <Textsnippet text="To achieve this goal, I completed a 16-week intensive software development bootcamp with CodeClan and 
-                am now looking for my next exciting challenge to continue to grow my skills. On this page, you will find a selection of my projects and experiences 
-                that demonstrate my adaptability and dedication to this field. 
-                Thank you for visiting, and please don't hesitate to contact me if you have any questions or opportunities for collaboration." />
-
-                <Textsnippet text="Thank you for visiting, and please don't hesitate to contact me if you have any questions or opportunities for collaboration." />
+                <Textsnippet text="This will be updated at irregular intervals to share my excitement, knowledge and learning around accessibility." />
+                
             </div>
         </main>
     )
