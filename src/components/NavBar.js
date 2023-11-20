246e40b34386c5b2d5ce0069850cedf0063cc590
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-20 12:46:15 +0000

    added logo, nav bar adjustment in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import './NavBar.css';
+import doroLogo from '../pictures/DoroLogo.png'
 
 export const NavBar=()=>{
 
     return(
         <header>
             <nav>
-                <Link to="/portfolio2.0" className="navlink">About</Link>
+                <Link to="/portfolio2.0" className="navlink">
+                    <img src={doroLogo} className="header-logo" alt="Doro"/>
+                    Home
+                </Link>
                 <Link to="/projects" className="navlink">Projects</Link>
                 <Link to="/skills" className="navlink">Skill Set</Link>
                 <Link to="/contact" className="navlink">Contact</Link>
