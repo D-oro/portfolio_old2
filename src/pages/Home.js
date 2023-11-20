b3246e2a7e2fa073385bf575c4b0484c1acf8fc1
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-20 10:00:30 +0000

    added logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export const Home=()=>{
     return(
        <main>
             <div clasName="homewrapper">
-                <h1>About</h1>
+                <h1>Doro</h1>
                 <p>Hello! I'm Doro, a software developer with a background in psychology and UX design, passionate about accessible code!</p>
                 <p>The funky font you see on my page is Open Dyslexic, a font specifically designed to remove barriers for dyslexic people. Here is a link to the <a href="https://opendyslexic.org/about">Open Dyslexic website</a> to learn more.</p>
             </div>
