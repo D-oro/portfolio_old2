f03acfea00c4b38a343c98ac28ed1447dca81358
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-23 18:54:16 +0000

    added icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,63 +5,76 @@ export const Skills=()=>{
         <main id="main">
             <section className="skillsbox">
                 <h1>Skill Set</h1>
-                <p>I'm experienced in UX/UI user-centred design, quantitative and 
-                    qualitative data collection and analysis, SPSS, and leading focus groups. 
-                    I'm also fluent in English, German and French.
-                </p>
-                    <h2>Main Languages</h2>
-                    <ul>
-                        <li>TypeScript</li>
-                        <li>Semantic HTML</li>
-                        <li>CSS & SCSS</li>
-                        <li>JavaScript</li>
-                        <li>Java</li>
-                        <li>Python</li>
-                        <li>SQL</li>
-                    </ul>
-                    <h2>Code writing & collaboration</h2>
-                    <ul>
-                        <li>Git & Github</li>
-                        <li>VSCode Liveshare</li>
-                        <li>IntelliJ Code With Me</li>
-                        <li>Agile methodologies</li>
-                        <li>Scrum-Ban</li>
-                        <li>Jira</li>
-                    </ul>
-                    <h2>Testing</h2>
-                    <ul>
-                        <li>Automated pipeline tests</li>
-                        <li>Unit testing</li>
-                        <li>Debugging & bug prevention</li>
-                        <li>Test Driven Development</li>
-                        <li>Playwright</li>
-                        <li>Jest</li>
-                        <li>Vitest</li>
-                    </ul>
+                    <article>
+                        <h2>Main Languages</h2>
+                        <ul>
+                            <li>TypeScript</li>
+                            <li>Semantic HTML</li>
+                            <li>CSS & SCSS</li>
+                            <li>JavaScript</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>SQL</li>
+                            <li>English<span aria-hidden> 🇬🇧</span></li>
+                            <li>German<span aria-hidden> 🇩🇪</span></li>
+                            <li>French<span aria-hidden> 🇫🇷</span></li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>Code writing & collaboration</h2>
+                        <ul>
+                            <li>Git & Github</li>
+                            <li>VSCode Liveshare</li>
+                            <li>IntelliJ Code With Me</li>
+                            <li>Agile methodologies</li>
+                            <li>Scrum-Ban</li>
+                            <li>Jira</li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>Testing</h2>
+                        <ul>
+                            <li>Automated pipeline tests</li>
+                            <li>Unit testing</li>
+                            <li>Debugging & bug prevention</li>
+                            <li>Test Driven Development</li>
+                            <li>Playwright</li>
+                            <li>Jest</li>
+                            <li>Vitest</li>
+                        </ul>
+                    </article>
+                    <article>
                     <h2>Frameworks & database management</h2> 
-                    <ul>
-                        <li>React</li>
-                        <li>Node.js</li>
-                        <li>NPM</li>
-                        <li>RESTful APIs</li>
-                        <li>Spring Boot</li>
-                        <li>PostgreSQL</li>
-                        <li>H2</li>
-                        <li>MariaD</li>
-                        <li>Spring</li>
-                        <li>Docker</li>
-                        <li>Postman</li>
-                        <li>Terraform</li>
-                        <li>Liquibase</li>
-                        <li>AWS cloud deployment</li>
-                    </ul>
-                    <h2>Diagrams & Visual Design</h2>
-                    <ul>
-                        <li>UML diagrams</li>
-                        <li>Wireframing</li>
-                        <li>GIMP image editing</li>
-                        <li>Excalidraw</li>
-                    </ul>
+                        <ul>
+                            <li>React</li>
+                            <li>Node.js</li>
+                            <li>NPM</li>
+                            <li>RESTful APIs</li>
+                            <li>Spring Boot</li>
+                            <li>PostgreSQL</li>
+                            <li>H2</li>
+                            <li>MariaD</li>
+                            <li>Spring</li>
+                            <li>Docker</li>
+                            <li>Postman</li>
+                            <li>Terraform</li>
+                            <li>Liquibase</li>
+                            <li>AWS cloud deployment</li>
+                        </ul>
+                        <h2>Diagrams & Visual Design</h2>
+                        <ul>
+                            <li>UML diagrams</li>
+                            <li>Wireframing</li>
+                            <li>GIMP image editing</li>
+                            <li>Excalidraw</li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>Users and Research </h2>
+                            <p>I'm experienced in UX/UI user-centred design, quantitative and 
+                            qualitative data collection and analysis, SPSS, and leading focus groups. 
+                            </p>
+                    </article>
             </section>
         </main>
     )
