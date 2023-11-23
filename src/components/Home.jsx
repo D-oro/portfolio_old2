56dbb6c7f14aaec67ba716e94ca36f4276974034
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-23 20:28:47 +0000

    linted

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <main id="main">
+      <section>
+        <h1>About</h1>
+        <p>
+          Hello! I&apos;m Doro, a software developer and accessibility
+          specialist with a background in psychology.
+        </p>
+        <p>
+          I enjoy speaking at meetups and conferences to share my
+          enthusiasm and knowledge, so get in touch if you want your audience
+          to learn about Accessibility, and how to write more inclusive code.
+        </p>
+      </section>
+    </main>
+  );
+}
+export default Home;
