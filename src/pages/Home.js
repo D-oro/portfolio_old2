45c8a96efde7d68721575d0e4647b04235e28e83
D-oro <107217252+D-oro@users.noreply.github.com>
2023-11-23 15:03:03 +0000

    changed about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,20 @@ import { Textsnippet } from "../components/Textsnippet";
 export const Home=()=>{
 
     return(
-       <main>
-            <div clasName="homewrapper">
-                <Textsnippet text="Hello and welcome to my portfolio! My name is Doro and I am a software developer with a unique background in psychology.
-                After earning my degree in psychology and working in mental health, I decided to make a career change and pursue my passion for technology." />
-
-                <Textsnippet text="To achieve this goal, I completed a 16-week intensive software development bootcamp with CodeClan and 
-                am now looking for my next exciting challenge to continue to grow my skills. On this page, you will find a selection of my projects and experiences 
-                that demonstrate my adaptability and dedication to this field. 
-                Thank you for visiting, and please don't hesitate to contact me if you have any questions or opportunities for collaboration." />
-
-                <Textsnippet text="Thank you for visiting, and please don't hesitate to contact me if you have any questions or opportunities for collaboration." />
-            </div>
-        </main>
-    )
+        <main>
+             <section>
+                 <h1>About</h1>
+                 <p>Hello! I'm Doro, a software developer and accessibility 
+                    specialist with a background in psychology.
+                     with a background in psychology 
+                 </p>
+                 <p>I enjoy speaking at meetups and conferences to share my 
+                     enthusiasm and knowledge, so get in touch if you want your audience
+                     to learn about Accessibility, and how to write more inclusive code.
+                 </p>
+             </section>
+         </main>
+     )
+ 
 };
 
